cfddb4989a32554b46f0e5d17ec8b0b60c34b84e
pekpuglia <pepuglia@gmail.com>
2023-11-16 10:08:02 +0100

    added todo

--- src/Propulsion.jl
+++ src/Propulsion.jl
@@ -1,4 +1,19 @@
 module Propulsion
+#todo
+#pipe flow
+#sub/supersonic choice - initial guesses depend on the variables
+#more tests
+#normal shocks?
+#actually use this - do As/Ac x Pt/Ps plot
+#tests with initial value
+#discober when initial guess for M is required
+#wrap and use supersonic::Bool?
+# q1dparams = Quasi1dimflowProperties(P=1e5, T=10.0, rho = 2.0, gamma = 1.4, Astar = 0.85, A = 1.0, initial_M=5)
+# q1dparams.M
+# res = Quasi1dimflowProperties(P = 1, T = 300, R = 287, gamma = 1.4, M=1.5, mdot=1)
+# res = Quasi1dimflowProperties(P = 100, T = 300, R = 287, gamma = 1.4, M=1.5, mdot=1)
+##
+# Quasi1dimflowProperties(P = 1u"bar", R = 287u"J/kg/K", gamma = 1.4, mdot = 1u"kg/s", T = 300u"K", A=0.3u"m^2", initial_M = 10).M
 
 include("physical_properties_base.jl")
 include("physical_properties.jl")
