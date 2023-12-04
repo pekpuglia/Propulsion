8a11c9f75892def4280051a71efaa81454236cc0
pekpuglia <pepuglia@gmail.com>
2023-12-04 12:03:14 +0100

    bug identified

--- src/Propulsion.jl
+++ src/Propulsion.jl
@@ -16,6 +16,8 @@ module Propulsion
 #actually use this - do As/Ac x Pt/Ps plot
 
 #add de Laval Nozzle - multiple sections
+
+#FlowProperties(P=1u"atm", MM=29u"g/mol", rho=1.225u"kg/m^3", M = 1.8, T=300u"K", initial_gamma = 1.4) - singular exception
 include("physical_properties_base.jl")
 include("physical_properties.jl")
 include("solver.jl")
