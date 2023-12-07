34493be9c508db6612c37a71998f595b9c2df77e
pekpuglia <pepuglia@gmail.com>
2023-12-07 17:11:34 +0100

    removed zombie code

--- src/Propulsion.jl
+++ src/Propulsion.jl
@@ -10,12 +10,6 @@ module Propulsion
 #wrap and use supersonic::Bool?
 #add de Laval Nozzle - multiple sections
 #make "default values" for initial_guesses
-# q1dparams = Quasi1dimflowProperties(P=1e5, T=10.0, rho = 2.0, gamma = 1.4, Astar = 0.85, A = 1.0, initial_M=5)
-# q1dparams.M
-# res = Quasi1dimflowProperties(P = 1, T = 300, R = 287, gamma = 1.4, M=1.5, mdot=1)
-# res = Quasi1dimflowProperties(P = 100, T = 300, R = 287, gamma = 1.4, M=1.5, mdot=1)
-##
-# Quasi1dimflowProperties(P = 1u"bar", R = 287u"J/kg/K", gamma = 1.4, mdot = 1u"kg/s", T = 300u"K", A=0.3u"m^2", initial_M = 10).M
 
 include("physical_properties_base.jl")
 include("physical_properties.jl")
