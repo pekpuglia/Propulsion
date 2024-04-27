380136a90bb872783a7d0c289024b8f6a0d51d9e
pekpuglia <pepuglia@gmail.com>
2024-04-27 19:27:49 +0200

    todo

--- src/Propulsion.jl
+++ src/Propulsion.jl
@@ -2,7 +2,7 @@ module Propulsion
 #todo
 
 #make "default values" for initial_guesses?
-#unit (use better units) and interval refactor!
+#unit (use better units)
 
 #pipe flow
 #add de Laval Nozzle - multiple sections
@@ -10,11 +10,11 @@ module Propulsion
 #sub/supersonic choice - initial guesses depend on the variables
 #discover when initial guess for M is required
 #wrap and use supersonic::Bool?
-#
+#interval refactor! - allow negative force
 
 #add ReTest
 
-#refactor find_clique
+#refactor find_clique - search only subgraphs
 #add more find_clique tests
 #inject clique guess
 
