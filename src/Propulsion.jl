9657525e50e82fb49b60b34d67ef4d413552ed24
pekpuglia <pepuglia@gmail.com>
2024-05-31 19:09:52 +0200

    updated todo

--- src/Propulsion.jl
+++ src/Propulsion.jl
@@ -4,19 +4,20 @@ module Propulsion
 #make "default values" for initial_guesses?
 #unit (use better units)
 
-#pipe flow
-#add de Laval Nozzle - multiple sections
 #actually use this - do As/Ac x Pt/Ps plot
 #sub/supersonic choice - initial guesses depend on the variables
 #discover when initial guess for M is required
 #wrap and use supersonic::Bool?
 
 #interval refactor! - allow negative force
+
 #maybe this solves NormalShock?
+#add entropy
+#add de Laval Nozzle - multiple sections
+#use p instead of variable capture 
 
 #add ReTest
 
-#refactor find_clique - search only subgraphs
 #add more find_clique tests
 #inject clique guess
 
